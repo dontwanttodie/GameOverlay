d7ac3414ed7e50f74a97adb455fd97e6c3e0dcf8
abevol <abevol@pm.me>
2022-10-26 02:07:38 +0800

    添加TestConsole

---
+++ TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestConsole
+{
+    using System.Globalization;
+
+    public class TestConsole
+    {
+        public static void Main()
+        {
+            CultureInfo culture = CultureInfo.CurrentUICulture;
+            Console.WriteLine("The current UI culture is {0} [{1}]",
+                culture.NativeName, culture.Name);
+        }
+    }
+}
