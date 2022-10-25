518c9d16e51fc0e72eca3013afb2c8fe34e970d8
abevol <abevol@pm.me>
2022-10-26 05:47:44 +0800

    测试多语言支持

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,14 +1,25 @@
 ﻿namespace TestConsole
 {
+    using System.Resources;
+    using System.Reflection;
     using System.Globalization;
 
-    public class TestConsole
+    public class Program
     {
         public static void Main()
         {
+            // Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
             CultureInfo culture = CultureInfo.CurrentUICulture;
-            Console.WriteLine("The current UI culture is {0} [{1}]",
-                culture.NativeName, culture.Name);
+            Console.WriteLine("The current UI culture is {0} [{1}]", culture.NativeName, culture.Name);
+
+
+            ResourceManager rm = new ResourceManager("TestConsole.Locales.Strings", Assembly.GetExecutingAssembly());
+            
+            var myName = rm.GetString("MyName");
+            Console.WriteLine("My name is: {0}", myName);
+
+            Console.WriteLine("Your name is: {0}", Locales.Strings.MyName);
+
         }
     }
 }
